71472b6b2dd2f8eabeb458e53d5b9dbf7c13b354
Haris Shah <harisshah385@gmail.com>
2023-07-02 17:49:04 +0500

    fix: add custom cors

--- api/src/server.ts
+++ api/src/server.ts
@@ -14,13 +14,31 @@ const allowedOrigins = [
   'https://lala-chappal.vercel.app',
   'http://localhost:3000',
 ]
-
-app.use(
-  cors({
-    origin: '*',
-  })
-)
-app.options('*', cors())
+app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'http://lala-chappal.vercel.app',
+    'https://lala-chappal.vercel.app',
+  ]
+  const origin = req.headers.origin
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin)
+  }
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
+  res.header('Access-Control-Allow-credentials', true)
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, UPDATE')
+  next()
+})
+// app.use(
+//   cors({
+//     origin: '*',
+//   })
+// )
+// app.options('*', cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
