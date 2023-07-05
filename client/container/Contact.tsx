acb5181e7675b10c5bafa591b9011ce6bbee8a1a
Haris Shah <harisshah385@gmail.com>
2023-07-05 11:07:34 +0500

    add cors

--- client/container/Contact.tsx
+++ client/container/Contact.tsx
@@ -20,7 +20,6 @@ export const Contact = () => {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
         },
         body: JSON.stringify(formData),
       })
